773d03cc290c7612c876633abf6bc9f3bc0ac03c
Mannie <mail@mannie.io>
2018-02-15 14:58:02 -0800

    Create project stub: AzureCognitive

---
+++ AzureCognitive/AzureCognitive/AzureCognitive.h
@@ -0,0 +1,19 @@
+//
+//  AzureCognitive.h
+//  AzureCognitive
+//
+//  Created by Mannie Tagarira on 2/15/18.
+//  Copyright © 2018 Microsoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AzureCognitive.
+FOUNDATION_EXPORT double AzureCognitiveVersionNumber;
+
+//! Project version string for AzureCognitive.
+FOUNDATION_EXPORT const unsigned char AzureCognitiveVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AzureCognitive/PublicHeader.h>
+
+
